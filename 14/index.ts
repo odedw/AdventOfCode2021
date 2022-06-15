1b00b11a8ca1e499338b5a54ad9975dde2901bd4
Oded Welgreen <oded.welgreen@gmail.com>
2022-06-15 09:34:57 -0500

    day 14 part 1

---
+++ 14/index.ts
@@ -0,0 +1,44 @@
+const input: string[] = require("./input.json");
+const initial: string = input[0];
+const rules = input
+  .slice(1)
+  .map((str) => str.split(" -> "))
+  .reduce((prev, curr) => {
+    prev[curr[0]] = [`${curr[0].substring(0, 1)}${curr[1]}`, `${curr[1]}${curr[0].substring(1, 2)}`];
+    return prev;
+  }, {});
+
+// part 1
+let pairCount = {};
+for (let i = 0; i < initial.length - 1; i++) {
+  const pair = initial.substring(i, i + 2);
+  pairCount[pair] = pairCount[pair] ? pairCount[pair] + 1 : 1;
+}
+
+const STEPS = 10;
+for (let i = 0; i < STEPS; i++) {
+  const newCount = {};
+  Object.keys(pairCount).forEach((pair) => {
+    const newPairs = rules[pair];
+    newPairs.forEach((p) => {
+      if (!newCount[p]) newCount[p] = 0;
+      newCount[p] += pairCount[pair];
+    });
+  });
+  pairCount = newCount;
+}
+
+const charCount = {};
+
+Object.keys(pairCount).forEach((pair) => {
+  if (!charCount[pair[0]]) charCount[pair[0]] = 0;
+  if (!charCount[pair[1]]) charCount[pair[1]] = 0;
+  charCount[pair[0]] += pairCount[pair];
+  charCount[pair[1]] += pairCount[pair];
+});
+Object.keys(charCount).forEach((c) => (charCount[c] = Math.ceil(charCount[c] / 2)));
+const sorted: any = Object.entries(charCount).sort((a: any, b: any) => a[1] - b[1]);
+const result = sorted[sorted.length - 1][1] - sorted[0][1];
+console.log(`=========================`);
+console.log(result);
+console.log(`=========================`);
