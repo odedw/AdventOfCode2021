345a5794eaa8e356e9436139d7305021477a1960
Oded Welgreen <oded.welgreen@gmail.com>
2022-06-22 09:41:45 -0500

    day 1 dotnet

---
+++ csharp/01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+
+var arr = JsonSerializer.Deserialize<int[]>(File.ReadAllText("./input.json"))!;
+
+// Part 1
+var count = arr.Where((number, i) => i > 0 && arr[i-1] < number)
+                .Count();
+Console.WriteLine(count);
+
+// Part 2
+count = arr.Where((number, i) => i > 0 && 
+                                 i < arr.Length - 2 && 
+                                 arr[i-1] + number + arr[i+1] < number + arr[i+1] + arr[i+2])
+            .Count();
+Console.WriteLine(count);
